774f0754710f256d8ba91956a4c2cfc0d0a15b9f
NepGliding <18307587985@163.com>
2026-04-12 15:00:03 +0800

    Twelfth archive 260412 1459

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
-<script setup></script>
-
 <template>
-  <router-view />
+  <n-flex justify="center" class="layout-wrapper">
+    <n-space vertical size="large">
+      <n-layout>
+        <n-layout-header> <AppHeader></AppHeader> </n-layout-header>
+        <n-layout-content> <router-view /> </n-layout-content>
+        <n-layout-footer> <AppFooter></AppFooter> </n-layout-footer>
+      </n-layout>
+    </n-space>
+  </n-flex>
 </template>
 
-<style scoped></style>
+<script setup>
+import { NFlex, NSpace, NLayout, NLayoutHeader, NLayoutContent, NLayoutFooter } from "naive-ui";
+import AppHeader from "@/components/layout/AppHeader.vue";
+import AppFooter from "@/components/layout/AppFooter.vue";
+</script>
+
+<style scoped>
+.layout-wrapper {
+  max-width: 1408px;
+  width: 100%;
+  margin: 0 auto;
+}
+.n-layout-header,
+.n-layout-footer {
+  background: rgba(128, 128, 128, 0.2);
+}
+
+.n-layout-sider {
+  background: rgba(128, 128, 128, 0.3);
+}
+
+.n-layout-content {
+  background: rgba(128, 128, 128, 0.4);
+}
+</style>
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <n-grid x-gap="12" :cols="2">
+    <n-gi>
+      <div class="light-green" />
+    </n-gi>
+    <n-gi>
+      <n-popover :overlap="overlap" :show-arrow="false" placement="bottom" trigger="click">
+        <template #trigger>
+          <n-button size="small" style="grid-area: 5 / 2 / 6 / 3"> Bottom </n-button>
+        </template>
+        <div class="large-text">啊！</div>
+      </n-popover>
+    </n-gi>
+  </n-grid>
+
+  <n-flex vertical>
+    <n-button @click="activate('left')"> 左 </n-button>
+    <n-drawer v-model:show="active" :width="368" :placement="placement">
+      <n-drawer-content title="斯通纳">
+        《斯通纳》是美国作家约翰·威廉姆斯在 1965 年出版的小说。
+      </n-drawer-content>
+    </n-drawer>
+  </n-flex>
+</template>
+
+<script setup>
+import { NFlex, NButton, NPopover, NDrawer, NDrawerContent, NGrid, NGi } from "naive-ui";
+
+import { ref } from "vue";
+
+const overlap = ref(false);
+
+const active = ref(false);
+const placement = ref("right");
+function activate(place) {
+  active.value = true;
+  placement.value = place;
+}
+</script>
+
+<style scoped></style>
