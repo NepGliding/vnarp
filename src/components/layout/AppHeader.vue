4c4b887d577b6ee4c6f9b11361943799da8ad681
NepGliding <18307587985@163.com>
2026-04-13 21:59:21 +0800

    Nineteenth archive 260413 2159

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -5,24 +5,36 @@
     </n-icon>
     <n-popover :overlap="overlap" :show-arrow="false" placement="bottom" trigger="click">
       <template #trigger>
-        <n-button size="small" style="grid-area: 5 / 2 / 6 / 3"> Bottom </n-button>
+        <n-button quaternary circle>
+          <Searchicon />
+        </n-button>
       </template>
-      <div class="large-text">啊！</div>
+
+      <div class="large-text">搜索</div>
     </n-popover>
-    <n-switch @update:value="handleToggleTheme">
-      <template #checked>🌙</template>
-      <template #unchecked>☀️</template>
-    </n-switch>
+    <n-popover :overlap="overlap" :show-arrow="false" placement="bottom" trigger="click">
+      <template #trigger>
+        <n-button quaternary circle>
+          <Functionicon />
+        </n-button>
+      </template>
+      <div class="large-text">功能</div>
+      <n-switch @update:value="handleToggleTheme"> </n-switch>
+    </n-popover>
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
 import { NButton, NPopover, NIcon, NSwitch } from "naive-ui";
 import logoSrc from "@/assets/images/awrw2_36.svg";
+import Searchicon from "@/assets/images/Searchicon.vue";
+import Functionicon from "@/assets/images/Functionicon.vue";
+
+//弹窗
 const overlap = ref(false);
 
+//主题切换
 const emit = defineEmits(["toggleTheme"]);
-
 const handleToggleTheme = () => {
   emit("toggleTheme");
 };
--- src/components/layout/AppNav.vue
+++ src/components/layout/AppNav.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="main">
     <n-button @click="activate('left')"> 左 </n-button>
-    <n-drawer v-model:show="active" :width="262" :placement="placement">
-      <n-drawer-content title="导航栏">
-        <router-link to="/">首页</router-link>
-        <router-link to="/article">文章</router-link>
-        <router-link to="/resources">资源</router-link>
-        <router-link to="/about">关于</router-link>
-      </n-drawer-content>
+    <n-drawer v-model:show="active" :width="257" :placement="placement">
+      <router-link to="/">首页</router-link>
+      <router-link to="/article">文章</router-link>
+      <router-link to="/resources">资源</router-link>
+      <router-link to="/about">关于</router-link>
     </n-drawer>
   </div>
 </template>
