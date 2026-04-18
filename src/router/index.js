740db6ac76f2f4cc0c34de081ed4c487388c9d76
NepGliding <18307587985@163.com>
2026-04-18 23:14:45 +0800

    Twenty-fifth archive 260418 2314

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
+import ResourcesPage from "@/Views/ResourcesPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/home", name: "home", component: () => import("@/Views/HomePage.vue") },
     { path: "/article", name: "article", component: () => import("@/Views/ArticlePage.vue") },
-    { path: "/", name: "resources", component: () => import("@/Views/ResourcesPage.vue") },
+    { path: "/", name: "resources", component: ResourcesPage },
   ],
 });
 
